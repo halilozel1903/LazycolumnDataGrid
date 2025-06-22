df9e339316ab4ecb43ed57ec4dd2b2e7e7b5b084
Halil Özel <halilozel1903@gmail.com>
2025-06-22 15:30:22 +0300

    Load people data from JSON assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.gson)
     implementation(libs.hilt.android)
     implementation(libs.coil.compose)
     kapt(libs.hilt.compiler)
