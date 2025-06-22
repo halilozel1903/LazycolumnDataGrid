5eb17c7413feb6c444584a028873d333e9b2e866
Halil Ozel <halilozel1903@gmail.com>
2025-06-22 16:12:28 +0300

    Bug fixed for MainActivity.kt

--- app/src/main/java/com/halil/ozel/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/recyclerviewsample/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import coil.compose.AsyncImage
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -117,6 +118,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun PersonDetail(person: Person, onBack: () -> Unit) {
         Scaffold(
